2585db849320d38da3c5bd44c5da090091631624
ZHZ <1262801184@qq.com>
2025-03-17 20:46:38 +0800

    Remove files from remote to free LFS quota

--- pointconv_pytorch/1112.py
+++
@@ -1,5 +0,0 @@
-import  h5py
-with h5py.File('I:/sandprocess/data/originnew/all_test_fps_subsets.h5', 'r') as f:
-    subsets = f['subsets'][:]  # 形状 (N, num_points, 6), N=batch_size
-    orig_labels = f['labels'][:]
-print(subsets)
